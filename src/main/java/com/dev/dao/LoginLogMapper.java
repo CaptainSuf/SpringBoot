2930adc2b29887b015cf8b90b0b6caf4659299fa
CaptainSuf <727473069@qq.com>
2018-06-24 23:13:37 +0800

    201806242313-添加mybatis逆向工厂配置

---
+++ src/main/java/com/dev/dao/LoginLogMapper.java
@@ -0,0 +1,17 @@
+package com.dev.dao;
+
+import com.dev.model.LoginLog;
+
+public interface LoginLogMapper {
+    int deleteByPrimaryKey(Integer loginLogId);
+
+    int insert(LoginLog record);
+
+    int insertSelective(LoginLog record);
+
+    LoginLog selectByPrimaryKey(Integer loginLogId);
+
+    int updateByPrimaryKeySelective(LoginLog record);
+
+    int updateByPrimaryKey(LoginLog record);
+}
